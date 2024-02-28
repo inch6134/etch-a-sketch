1b17d5d4e25665a1847200bedd7c6c627afff4d4
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-28 11:17:29 -0800

    Link brush color to selected color

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 const canvasDOM = document.querySelector("#canvas");
 const sliderDOM = document.querySelector("input");
 const sliderPane = document.querySelector("#slider-pane");
-const clearButton = document.querySelector('#clear');
+const clearButton = document.querySelector("#clear");
+const colorSelector = document.querySelector("#color-selector");
 
 let sliderValue = undefined;
 let gridSquares = [];
+let color = "";
 
 initialize();
 
@@ -13,85 +15,59 @@ function initialize() {
   sliderValue = sliderDOM.value;
   generateGrid();
   sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;
+  let style = getComputedStyle(colorSelector);
+  color = style.getPropertyValue("background-color");
 }
+
+console.log(color);
 
 // utility functions
 
-function generateGrid () {
+function generateGrid() {
   gridSquares.length = 0;
   empty(canvasDOM);
   for (let i = 0; i < sliderValue; i++) {
     gridSquares[i] = [];
     for (let j = 0; j < sliderValue; j++) {
       const grid = document.createElement("div");
-      grid.innerHTML = `Grid`;
       grid.style.width = `${canvasDOM.clientWidth / sliderValue}px`;
       grid.style.height = `${canvasDOM.clientHeight / sliderValue}px`;
       grid.setAttribute("class", "grid-square");
       canvasDOM.append(grid);
       gridSquares[i][j] = grid;
     }
-  }  
+  }
+  enableEditing();
 }
 
 function empty(element) {
-  while(element.firstElementChild) {
-     element.firstElementChild.remove();
+  while (element.firstElementChild) {
+    element.firstElementChild.remove();
   }
 }
 
 function setValue() {
   sliderValue = sliderDOM.value;
-  sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;  
+  sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;
 }
 
 // event listeners
 
-clearButton.addEventListener('click', generateGrid);
+clearButton.addEventListener("click", generateGrid);
 
-sliderDOM.addEventListener('input', setValue);
+sliderDOM.addEventListener("input", setValue);
 
-sliderDOM.addEventListener('mouseup', () => {
+sliderDOM.addEventListener("mouseup", () => {
   setValue();
   generateGrid();
-})
-
-gridSquares.forEach((row) => {
-  row.forEach((column) => {
-    column.addEventListener('mouseover', () => {
-      column.innerHTML = '';
-    });
-  });
 });
 
-// sliderPane.addEventListener('click', () => {
-//   sliderPane.innerHTML = sliderPaneInput;
-// });
-
-// gridSquares[0][0].addEventListener("hover", () => {
-//     gridSquares[0][0].removeAttribute("class");
-//     gridSquares[0][0].style.backgroundColor = "black";
-// });
-
-// event handler for drawing on tiles
-// for (let i = 0; i < sliderValue; i++) {
-//   for (let j = 0; j < sliderValue; j++) {
-//     const square = gridSquares[i][j];
-//     console.log(square);
-//     square.addEventListener("hover", () => {
-//       square.removeAttribute("class");
-//       square.style.backgroundColor = "black";
-//     });
-//   }
-// }
-
-// canvasDOM.querySelectorAll('div').forEach(element => {
-//     element.addEventListener('hover', () => {
-//         element.removeAttribute("class");
-//         element.style.backgroundColor = "black";
-//     })
-// })
-
-// event handler for grid size slider
-
-//
+function enableEditing() {
+  gridSquares.forEach((row) => {
+    row.forEach((column) => {
+      column.addEventListener("mouseover", () => {
+        column.style.backgroundColor = color;
+      });
+    });
+  });
+}
