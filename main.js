8615f041d1608523f8d24b3da9fb24004f08cf16
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-28 10:58:32 -0800

    Add event listener to change grid on hover

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ const sliderDOM = document.querySelector("input");
 const sliderPane = document.querySelector("#slider-pane");
 const clearButton = document.querySelector('#clear');
 
-
-let sliderValue = sliderDOM.value;
+let sliderValue = undefined;
 let gridSquares = [];
 
-generateGrid();
+initialize();
+
+function initialize() {
+  sliderValue = sliderDOM.value;
+  generateGrid();
+  sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;
+}
+
+// utility functions
 
 function generateGrid () {
   gridSquares.length = 0;
+  empty(canvasDOM);
   for (let i = 0; i < sliderValue; i++) {
     gridSquares[i] = [];
     for (let j = 0; j < sliderValue; j++) {
@@ -26,11 +34,39 @@ function generateGrid () {
   }  
 }
 
-sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;
+function empty(element) {
+  while(element.firstElementChild) {
+     element.firstElementChild.remove();
+  }
+}
 
-clearButton.addEventListener('click', () => {
-  grid.setAttribute("class", "grid-square");
+function setValue() {
+  sliderValue = sliderDOM.value;
+  sliderPane.innerHTML = `${sliderValue} x ${sliderValue}`;  
+}
+
+// event listeners
+
+clearButton.addEventListener('click', generateGrid);
+
+sliderDOM.addEventListener('input', setValue);
+
+sliderDOM.addEventListener('mouseup', () => {
+  setValue();
+  generateGrid();
+})
+
+gridSquares.forEach((row) => {
+  row.forEach((column) => {
+    column.addEventListener('mouseover', () => {
+      column.innerHTML = '';
+    });
+  });
 });
+
+// sliderPane.addEventListener('click', () => {
+//   sliderPane.innerHTML = sliderPaneInput;
+// });
 
 // gridSquares[0][0].addEventListener("hover", () => {
 //     gridSquares[0][0].removeAttribute("class");
